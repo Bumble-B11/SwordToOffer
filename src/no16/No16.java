5e1c165232df95360a14caa42dc929c66054b6f4
wangsikai@cmcm.com <wangsikai@cmcm.com>
2019-11-08 23:18:47 +0800

    No16 反转链表

---
+++ src/no16/No16.java
@@ -0,0 +1,33 @@
+package no16;
+
+import data.ListNode;
+import utils.LinkedListUtils;
+
+/**
+ * Created by bumblebee on 2019/11/8.
+ */
+public class No16 {
+
+    public static void main(String[] args) {
+        ListNode head = LinkedListUtils.createList(new int[]{1, 2, 3, 4, 5});
+
+        LinkedListUtils.printList(new No16().reverseLinkerList(head));
+    }
+
+    private ListNode reverseLinkerList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = null, next;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+}
