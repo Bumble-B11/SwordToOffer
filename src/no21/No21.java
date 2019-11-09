e5be1782fa30d7085739696c5b87786d7e7c05c3
wangsikai@cmcm.com <wangsikai@cmcm.com>
2019-11-09 17:40:59 +0800

    No21 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。

---
+++ src/no21/No21.java
@@ -0,0 +1,45 @@
+package no21;
+
+import java.util.Stack;
+
+/**
+ * Created by bumblebee on 2019/11/9.
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ */
+public class No21 {
+    private Stack<Integer> mDataStack;
+    private Stack<Integer> mMinStack;
+
+    public No21() {
+        mDataStack = new Stack<>();
+        mMinStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        mDataStack.push(x);
+        Integer min = getMin();
+        if (x <= min) {
+            mMinStack.push(x);
+        }/* else {
+            mMinStack.push(min);
+        }*/
+    }
+
+    public void pop() {
+        int x = mDataStack.pop();
+        if (x == getMin()) {
+            mMinStack.pop();
+        }
+    }
+
+    public int top() {
+        return mDataStack.peek();
+    }
+
+    public int getMin() {
+        if (mMinStack.isEmpty()) {
+            return Integer.MAX_VALUE;
+        }
+        return mMinStack.peek();
+    }
+}
